e3f223e27f0ada5b0509b7a9f622ae09c70e49a9
purpl3-yt <remezdevid2000@gmail.com>
2023-08-02 19:53:23 +0300

    update script

--- tweaks/DeleteOneDrive.bat
+++ tweaks/DeleteOneDrive.bat
@@ -1,6 +1,33 @@
-taskkill /f /im OneDrive.exe & %systemroot%\SysWOW64\OneDriveSetup.exe /uninstall
-reg delete HKCR\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /f
-reg delete HKCR\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6} /f
-rd /s /q %userprofile%\OneDrive
-rd /s /q %userprofile%\AppData\Local\Microsoft\OneDrive
-rd /s /q "%allusersprofile%\Microsoft OneDrive"
+@echo off
+cls
+
+set x86="%SYSTEMROOT%\System32\OneDriveSetup.exe"
+set x64="%SYSTEMROOT%\SysWOW64\OneDriveSetup.exe"
+
+echo Closing OneDrive process.
+echo.
+taskkill /f /im OneDrive.exe > NUL 2>&1
+ping 127.0.0.1 -n 5 > NUL 2>&1
+
+echo Uninstalling OneDrive...
+echo.
+if exist %x64% (
+%x64% /uninstall
+) else (
+%x86% /uninstall
+)
+
+echo Removing OneDrive leftovers...
+echo.
+rd "%USERPROFILE%\OneDrive" /Q /S > NUL 2>&1
+rd "C:\OneDriveTemp" /Q /S > NUL 2>&1
+rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S > NUL 2>&1
+rd "%LOCALAPPDATA%\OneDrive" /Q /S > NUL 2>&1
+rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S > NUL 2>&1 
+
+echo Removing OneDrive from the Explorer Side Panel...
+echo.
+REG DELETE "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
+REG DELETE "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f > NUL 2>&1
+
+:: Script taken from https://github.com/hellzerg/optimizer
